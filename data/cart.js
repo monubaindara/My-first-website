369c9cbbdc5ba6e73974fd811803e2df9d708c67
monubaindara <monubaindara@gmail.com>
2026-01-11 13:27:43 +0530

    Add files via upload

---
+++ data/cart.js
@@ -0,0 +1,69 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart = [
+  {
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId:'3'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId:'2'
+  }];
+}
+
+function saveToStroage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+export function addTOCart(productId){
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId:'2'
+      }); 
+    }
+    saveToStroage();
+};
+
+export function removeFromCart(productId){
+ const newCart = [];
+
+ cart.forEach((cartItem) => { 
+  if(productId !== cartItem.productId){
+    newCart.push(cartItem);
+  }
+ });
+ cart = newCart; 
+  
+ saveToStroage();
+};
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.deliverOptionId = deliveryOptionId;
+    saveToStroage();
+  }
+  
+  
+
+}
+ 
